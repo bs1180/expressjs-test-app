38b8e10b8ddcb7d719e63fc3e8cf441356f32582
Ben Smith <benedictsmith@gmail.com>
2023-12-02 22:15:07 +0100

    move log

--- node.js
+++ node.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log(process.env);
+
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-console.log(process.env);
